7298552622fc7eb191545163d30b81f56e1107d7
Ngapa <krilinamar@gmail.com>
2022-09-18 21:48:08 +0700

    sett

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,8 @@
 <?php
-    $koneksi = mysqli_connect("localhost", "root", "", "db_sensor");
+    $host = "iot.cmocelv8wfcs.us-west-1.rds.amazonaws.com";
+    $user = "admin";
+    $pass = "admin1234";
+    $db = "waterplant";
+    $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 ?>
