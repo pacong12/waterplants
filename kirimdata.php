590b5052292449cf59177f158c4d8574c710c496
Ngapa <krilinamar@gmail.com>
2022-09-18 22:08:16 +0700

    fix

--- kirimdata.php
+++ kirimdata.php
@@ -1,17 +1,17 @@
 <?php 
 
 // koneksi database
-$koneksi = mysqli_connect("localhost", "root", "", "db_sensor");
+include "./koneksi.php";
 
 // menangkap parameter yang dikirim oleh esp 32
 $kelembaban = $_GET['kelembaban'];
 
-// simpan ke tb_sensor
+// simpan ke sensor
 // atur ID dimulai dari 1
-mysqli_query($koneksi,"ALTER TABLE tb_sensor AUTO_INCREMENT = 1" );
+mysqli_query($koneksi,"ALTER TABLE sensor AUTO_INCREMENT = 1" );
 
 // simpan nilai kelembaban ke table sensor
-$simpan = mysqli_query($koneksi, "insert into tb_sensor(kelembaban)values ('$kelembaban')");
+$simpan = mysqli_query($koneksi, "insert into sensor(kelembaban)values ('$kelembaban')");
 
 // memberi respon ke esp 32
 if($simpan)
